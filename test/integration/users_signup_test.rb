faed32b38579ff35cee2ec25cecc2a20df0a8a38
Rob Maclean <rjmac@live.co.uk>
2017-11-10 07:08:15 +0000

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+      ActionMailer::Base.deliveries.clear
+  end
 
   test "invalid signup information" do
       get signup_path
@@ -18,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'div.field_with_errors'
   end
 
-  test "valid signup inoformation" do
+  test "valid signup inoformation with the account activation" do
       get signup_path
       assert_difference 'User.count', 1 do
           post users_path, params: { user: { name:  "Example User",
@@ -26,9 +29,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "password",
                                          password_confirmation: "password" } }
       end
+      assert_equal 1, ActionMailer::Base.deliveries.size
+      user = assigns(:user)
+      assert_not user.activated?
+      # Try to log in before activation.
+      log_in_as(user)
+      assert_not is_logged_in?
+      # invalid activation token
+      get edit_account_activation_path("invalid token", email: user.email)
+      assert_not  is_logged_in?
+      #valid token, wrong email
+      get edit_account_activation_path(user.activation_token, email: "wrong")
+      assert_not is_logged_in?
+      # Valid activation token
+      get edit_account_activation_path(user.activation_token, email: user.email)
+      assert user.reload.activated?
       follow_redirect!
       assert_template 'users/show'
-      assert_not flash.nil?
+      assert is_logged_in?
+    #   assert_not flash.nil?
    end
 
 
