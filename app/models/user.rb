66d558364eaeb266583874d41267e6849e2d8720
Rob Maclean <rjmac@live.co.uk>
2017-11-18 12:56:54 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@
 
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships,  class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+                                     foreign_key: "followed_id",
+                                     dependent:   :destroy
+    has_many :following, through: :active_relationships,  source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
 
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
@@ -84,9 +92,35 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
-    # Defines a proto-feed.
+    # Returns a user's status feed.
     def feed
-        Micropost.where("user_id = ?", id)
+        # Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+        #                  following_ids: following_ids, user_id: id)
+
+        following_ids = "SELECT followed_id FROM relationships
+                          WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                         OR user_id = :user_id", user_id: id)
+    end
+
+    # # Defines a proto-feed.
+    # def feed
+    #     Micropost.where("user_id = ?", id)
+    # end
+
+    # Follows a user.
+    def follow(other_user)
+      following << other_user
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
     end
 
     private
@@ -101,4 +135,6 @@ class User < ApplicationRecord
         self.activation_token = User.new_token
         self.activation_digest = User.digest(activation_token)
     end
+
+
 end
