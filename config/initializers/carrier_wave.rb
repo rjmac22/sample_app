adc1e5d6c1fa2f42348f1a76cd8ae2ec830e3d5a
Rob Maclean <rjmac@live.co.uk>
2017-11-20 13:32:19 +0000

    Minor changes and AWS for pictures

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,14 @@
+
+
+
 if Rails.env.production?
-    CarrierWave.configure do |config|
-        config.fog_credentials = {
-            # Configuration for Amazon S3
-            :provider              => 'AWS'
-            :aws_access_key_id     => ENV['AKIAJADO52QCT3JTV4FQ'],
-            :aws_secret_access_key => ENV['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']
-        }
-        config.fog_directory = ENV['S3_BUCKET']
-    end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAJADO52QCT3JTV4FQ'],
+      :aws_secret_access_key => ENV['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']
+    }
+    config.fog_directory     =  ENV['micropostrjmac22']
+  end
 end
