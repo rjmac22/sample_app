a9e4e30a3ab34d73ea7586d0eab2db045cc8ff1e
Rob Maclean <rjmac@live.co.uk>
2017-11-20 13:26:09 +0000

    Minor changes and AWS for pictures

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
         configure.fog_credentials = {
             # Configuration for Amazon S3
             :provider              => 'AWS'
-            :aws_access_key_id     => ENV['AKIAJADO52QCT3JTV4FQ']
-            :aws_secret_access_key => ENV['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']
+            :aws_access_key_id     => ENV['AKIAJADO52QCT3JTV4FQ'],
+            :aws_secret_access_key => ENV['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'],
         }
         config.fog_directory = ENV['S3_BUCKET']
     end
